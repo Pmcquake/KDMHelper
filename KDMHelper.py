250dfdc92ba758e6f40e1d8b35c00d5a55b84ff3
Peter McCurry <pmcquake@gmail.com>
2017-10-16 00:00:06 -0400

    Working tests for queries with or without a WHERE statement

--- KDMHelper.py
+++ KDMHelper.py
@@ -17,28 +17,53 @@ cur = db.cursor()
 #---------------------------------------------------------------------------------------------------------------------------------------------#
 
 # display the return of a mySQLdb query using a Tkinter text box
-def getItem():
-	cur.execute("SELECT * FROM weapons")
-	wep = ""
-	text = Text(root, width = 100)
+def getItem(select, table, where, textBox):
+	query = "SELECT " + select + " FROM " + table
+	if (where == None):												# if there is no WHERE statement
+		cur.execute(query)
+		items = ""
+		for row in cur.fetchall():
+			for col in row[1:]:
+				items += str(col) + " | "
+			items += "\n"
+		textBox.delete(1.0, END)									# clear text box before writing new contents
+		textBox.insert(INSERT, items)
+		textBox.pack()
+	else:															# if there is a WHERE statement
+		query += " WHERE " + where
+		getWhere(query, textBox)
+
+
+# takes care of queries when there is a WHERE statement
+def getWhere(query, textBox):
+	cur.execute(query)
+	items = ""
 	for row in cur.fetchall():
 		for col in row[1:]:
-			wep += str(col) + " | "
-	text.insert(INSERT, wep)
-	text.pack()
+			items += str(col) + " | "
+		items += "\n"
+	textBox.delete(1.0, END)										# clear text box before writing new contents
+	textBox.insert(INSERT, items)
+	textBox.pack()
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------------#
 
 # Create Tkinter GUI
 root = Tkinter.Tk()
+textBox = Text(root, width = 150)
 
 # Button for testing out GUI
-testButton = Tkinter.Button(root, text = "Test", command = getItem)
-
+weaponTest = Tkinter.Button(root, text = "Weapons", command = lambda: getItem("*", "weapons", None, textBox))
+oneWeapon = Tkinter.Button(root, text = "One Weapon", command = lambda: getItem("*", "weapons", "id = 1", textBox))
+armorTest = Tkinter.Button(root, text = "Armor", command = lambda: getItem("*", "armor", None, textBox))
+gearTest = Tkinter.Button(root, text = "Gear", command = lambda: getItem("*", "gear", None, textBox))
 
 # Actually open the GUI
-testButton.pack()
+weaponTest.pack()
+oneWeapon.pack()
+armorTest.pack()
+gearTest.pack()
 root.mainloop()
 
 
